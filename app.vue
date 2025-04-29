7a83bb1a87f09d36e2cd366a2e5f5ed65492e6f2
Avery Burke <DrFroid01@gmail.com>
2025-04-28 21:56:09 -0700

    removed some boiler plat to verify CD in netlify

--- app.vue
+++ app.vue
@@ -1,12 +1,12 @@
 <script setup>
 useHead({
   htmlAttrs: { lang: "en-US" },
-  title: "Nuxt Starter",
+  title: "Avery's Nuxt Test",
   meta: [
     {
       name: "description",
       content:
-        "Nuxt Starter is a minimal template created for quick landing pages, built with Nuxt and Tailwind CSS.",
+        "Setting up a nuxt app on Netlify with dynamo DB.",
     },
   ],
   bodyAttrs: {
