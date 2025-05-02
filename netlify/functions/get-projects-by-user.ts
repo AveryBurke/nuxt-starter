f730f0079b9fe7158553fdfc9449c0fadfa49485
Avery Burke <DrFroid01@gmail.com>
2025-05-01 17:26:20 -0700

    returns a response

--- netlify/functions/get-projects-by-user.ts
+++ netlify/functions/get-projects-by-user.ts
@@ -1,6 +1,4 @@
-import { type Config, type HandlerContext, type Handler, type HandlerEvent } from "@netlify/functions"
-// import { DynamoDB } from "aws-sdk";
-
+import { type Config, type HandlerContext, type Handler, type HandlerEvent, type HandlerResponse } from "@netlify/functions"
 
 interface ExtendedHandlerContext extends HandlerContext {
     params?: {
@@ -9,19 +7,25 @@ interface ExtendedHandlerContext extends HandlerContext {
     };
 };
 
-const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    'Access-Control-Allow-Methods': 'GET, OPTIONS',
-    'Content-Type': 'application/json'
-};
-
-export default async (event: HandlerEvent, context: ExtendedHandlerContext) => {
+export const handler = async (event: HandlerEvent, context: ExtendedHandlerContext): Promise<Response> => {
     const userID = context.params?.userID
-    console.log({ userID })
-    return { statusCode: 200, headers, body: userID }
-};
 
+    console.log("Function called with userID:", userID);
+
+    // Use the Response object format instead of the older object format
+    return new Response(
+        JSON.stringify({ userID }),
+        {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Access-Control-Allow-Methods': 'GET, OPTIONS'
+            }
+        }
+    );
+};
 
 export const config: Config = {
     path: '/api/users/:userID'
